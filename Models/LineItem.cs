a9bdfc10ca464743a62aa219ce5911074c614252
b-zapata <b.zapata.n@gmail.com>
2024-04-08 15:41:16 -0600

    Populated basic fields of products, orders, and lineItems

--- Models/LineItem.cs
+++ Models/LineItem.cs
@@ -2,5 +2,9 @@
 {
     public class LineItem
     {
+        public int transaction_ID { get; set; }
+        public int product_ID { get; set; }
+        public int qty { get; set; }
+        public decimal rating { get; set; }
     }
 }
--- Models/Product.cs
+++ Models/Product.cs
@@ -2,5 +2,16 @@
 {
     public class Product
     {
+        public int product_ID {  get; set; }
+        public string name { get; set; }
+        public int year { get; set; }
+        public int num_parts { get; set; }
+        public int price { get; set; }
+        public string img_link { get; set; }
+        public string primary_color { get; set; }
+        public string secondary_color { get; set; }
+        public string description { get; set; }
+        public string category { get; set; }
+
     }
 }
