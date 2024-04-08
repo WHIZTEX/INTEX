a5ba33e5ba09aab2d182366cca2b92f5ddc4f5e4
Braden Toone <braden@toonetown.com>
2024-04-08 11:18:45 -0600

    Added Cookie Consent Notification

--- Program.cs
+++ Program.cs
@@ -23,9 +23,9 @@ builder.Services.Configure<CookiePolicyOptions>(options =>
 {
     // This lambda determines whether user consent for non-essential 
     // cookies is needed for a given request.
-    options.CheckConsentNeeded = context => true;
-
+    options.CheckConsentNeeded = _ => true;
     options.MinimumSameSitePolicy = SameSiteMode.None;
+    options.ConsentCookieValue = "true";
 });
 
 // Configuring additional password requirements
