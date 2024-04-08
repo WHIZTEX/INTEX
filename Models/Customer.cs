b232e26f46e3f36a3454c41ae5bf30bb43a0ec56
b-zapata <b.zapata.n@gmail.com>
2024-04-08 14:45:44 -0600

    resolving conflicts

---
+++ Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace INTEX.Models
+{
+    public class Customer
+    {
+        public int customer_ID { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public DateTime birth_date { get; set; } // Might have to handle this once we deploy and connect to database
+        public string country_of_residence { get; set; }
+        public string gender { get; set; }
+        public decimal age { get; set; } // Do we want to calculate this dinamically somehow?
+    }
+}
