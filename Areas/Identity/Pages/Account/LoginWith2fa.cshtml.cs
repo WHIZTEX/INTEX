1066af0b9279d0c00742f9ad0a3ecaf982ce87b1
Braden Toone <braden@toonetown.com>
2024-04-10 10:25:19 -0600

    Regenerated identity area with existing dbcontext options

--- Areas/Identity/Pages/Account/LoginWith2fa.cshtml.cs
+++ Areas/Identity/Pages/Account/LoginWith2fa.cshtml.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using INTEX.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Logging;
 
@@ -16,13 +17,13 @@ namespace INTEX.Areas.Identity.Pages.Account
 {
     public class LoginWith2faModel : PageModel
     {
-        private readonly SignInManager<IdentityUser> _signInManager;
-        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<Customer> _signInManager;
+        private readonly UserManager<Customer> _userManager;
         private readonly ILogger<LoginWith2faModel> _logger;
 
         public LoginWith2faModel(
-            SignInManager<IdentityUser> signInManager,
-            UserManager<IdentityUser> userManager,
+            SignInManager<Customer> signInManager,
+            UserManager<Customer> userManager,
             ILogger<LoginWith2faModel> logger)
         {
             _signInManager = signInManager;
--- Areas/Identity/Pages/Account/Manage/SetPassword.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/SetPassword.cshtml.cs
@@ -5,6 +5,7 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
+using INTEX.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -13,12 +14,12 @@ namespace INTEX.Areas.Identity.Pages.Account.Manage
 {
     public class SetPasswordModel : PageModel
     {
-        private readonly UserManager<IdentityUser> _userManager;
-        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<Customer> _userManager;
+        private readonly SignInManager<Customer> _signInManager;
 
         public SetPasswordModel(
-            UserManager<IdentityUser> userManager,
-            SignInManager<IdentityUser> signInManager)
+            UserManager<Customer> userManager,
+            SignInManager<Customer> signInManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
